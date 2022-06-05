551721e09698d88d467d5f95cd70b9fecc2670fa
moohabir <87937848+moohabir@users.noreply.github.com>
2022-06-05 21:53:30 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <div className="header">
+        <h1>Header</h1>
+      </div>
+
+      <div className="row">
+        <h1 className="column1">column 1</h1>
+        <h1 className="column2">Column 2</h1>
+        <h1 className="column3">column 3</h1>
+      </div>
+    </div>
+  );
+}
