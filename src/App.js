bacd0540eb7e2139f4b476bb2d7d799caf94cbeb
moohabir <87937848+moohabir@users.noreply.github.com>
2022-06-06 14:16:26 +0200

    3r

--- src/App.js
+++ src/App.js
@@ -11,6 +11,10 @@ export default function App() {
         <h1 className="column1">column 1</h1>
         <h1 className="column2">Column 2</h1>
         <h1 className="column3">column 3</h1>
+        <h1 className="column4">column 4</h1>
+      </div>
+      <div className="footer">
+        <h1>Footer</h1>
       </div>
     </div>
   );
